d119ed03404d38c54bff796df44641b9d64b11b1
Ralf Wilke <ralf@ralfwilke.com>
2016-10-28 13:25:23 +0000

    Changed startslave.sh to display gui

--- software/raspager-java/startslave.sh
+++ software/raspager-java/startslave.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/pi/RasPagerDigi/software/raspager-java
-sudo java -jar  -classpath jna-4.1.0.jar:. -jar raspager.jar  -nogui -configfile=/home/pi/RasPagerDigi/software/raspager-java/db0eif.cfg
+sudo java -jar  -classpath jna-4.1.0.jar:. -jar raspager.jar -configfile=/home/pi/RasPagerDigi/software/raspager-java/db0eif.cfg -v
