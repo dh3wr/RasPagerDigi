02b78343a188f4a6b2d1efc27373fa122e21c9b9
Ralf Wilke <ralf@ralfwilke.com>
2016-10-31 13:14:59 +0000

    Changed unsigend long to signed long in sendByte of radiolink.cpp

--- software/radiolib/src/radiolink.cpp
+++ software/radiolib/src/radiolink.cpp
@@ -28,9 +28,9 @@
 // Radio Parameters
 #define CRYSTALFREQ 4915200UL
 #define FREQUENCY 439987500UL     
-    
+
 #define ADF7012_CRYSTAL_DIVIDER 1 
-    
+
 // POCSAG Protokoll
 #define POCSAG_PREAMBLE_CODEWORD 0xAAAAAAAA
 #define POCSAG_IDLE_CODEWORD 0x7A89C197
@@ -56,53 +56,52 @@ extern "C" RadioAdf7012 * setup() {
 
   digitalWrite(ATDATA, LOW);
   digitalWrite(ATCLK, LOW);
-    
   RadioAdf7012* radio = new RadioAdf7012();
   (*radio).setup();
   return radio;
 }
 
 extern "C" bool pttOn(RadioAdf7012 * radio) {
-  
+
   int counter = 0;
   bool status = false;
 
   do {
     status = radio->ptt_on();
-    
   } while(!status && counter++ <10);  
   return status;
 }
-  
+
 extern "C" void pttOff(RadioAdf7012 * radio) {
  
   while(digitalRead(PTT) == 1) {
     delay(100);
   }
-	  
+
   digitalWrite(ATDATA, LOW);
   digitalWrite(ATCLK, LOW);
-    
+
   radio->ptt_off();
 }
 
-extern "C" bool sendByte(unsigned long data) {
- 
+// Changes from Ralf Wilke 31.10.16: Was unsigned long, now (signed) long
+extern "C" bool sendByte(long data) {
+
   for(int c = 31; c>=0;c--){
-	    
+
     while(digitalRead(HANDSHAKE) == 0) {
       delayMicroseconds(100);
     }
-	  
+
     if ((data & (unsigned long)(1 << c)) >> c)
 	digitalWrite(ATDATA, HIGH);
     else
 	digitalWrite(ATDATA, LOW);
-		
+
     delayMicroseconds(20);
     digitalWrite(ATCLK, HIGH);
     delayMicroseconds(100);
     digitalWrite(ATCLK, LOW);
     delayMicroseconds(50);
-  }	  
+  }
 }
