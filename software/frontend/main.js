9cc27c6a48d1814e28c6e63347e3f9da1528273d
Ralf Wilke <ralf@ralfwilke.com>
2017-01-24 11:39:12 +0000

    All JS in one file

--- software/frontend/main.js
+++ software/frontend/main.js
@@ -75,11 +75,12 @@ var vm = new Vue({
         send: function(data) {
             this.socket.send(JSON.stringify(data));
         },
-//        restart: function(event) {
-//            this.send("Restart");
-//        },
+/*
+        restart: function(event) {
+            this.send("Restart");
+        },
         
-/*        send_message: function(event)  {
+        send_message: function(event)  {
             localStorage && (localStorage.pager_addr = this.addr);
 
             var req = {"SendMessage": {
@@ -94,4 +95,114 @@ var vm = new Vue({
     }
 });
 
+$(function () {
+
+    var gaugeOptions = {
+
+        chart: {
+            type: 'solidgauge'
+        },
+
+        title: null,
+
+        pane: {
+            center: ['50%', '85%'],
+            size: '140%',
+            startAngle: -90,
+            endAngle: 90,
+            background: {
+                backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+                innerRadius: '60%',
+                outerRadius: '100%',
+                shape: 'arc'
+            }
+        },
+
+        tooltip: {
+            enabled: false
+        },
+
+        // the value axis
+        yAxis: {
+            stops: [
+                [0.1, '#55BF3B'], // green
+                [0.5, '#DDDF0D'], // yellow
+                [0.9, '#DF5353'] // red
+            ],
+            lineWidth: 0,
+            minorTickInterval: null,
+            tickAmount: 2,
+            title: {
+                y: -70
+            },
+            labels: {
+                y: 16
+            }
+        },
+
+        plotOptions: {
+            solidgauge: {
+                dataLabels: {
+                    y: 5,
+                    borderWidth: 0,
+                    useHTML: true
+                }
+            }
+        }
+    };
+
+    // The voltage gauge
+    var chartVoltage = Highcharts.chart('container-voltage', Highcharts.merge(gaugeOptions, {
+        yAxis: {
+            min: 0,
+            max: 16,
+            title: {
+                text: 'Voltage'
+            }
+        },
+
+        credits: {
+            enabled: false
+        },
+
+        series: [{
+            name: 'Voltage',
+            data: [80],
+            dataLabels: {
+                format: '<div style="text-align:center"><span style="font-size:25px;color:' +
+                    ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' +
+                       '<span style="font-size:12px;color:silver">V</span></div>'
+            },
+            tooltip: {
+                valueSuffix: ' V'
+            }
+        }]
+
+    }));
+
+    // The Ampere gauge
+    var chartAmpere = Highcharts.chart('container-ampere', Highcharts.merge(gaugeOptions, {
+        yAxis: {
+            min: 0,
+            max: 10,
+            title: {
+                text: 'Ampere'
+            }
+        },
+
+        series: [{
+            name: 'Ampere',
+            data: [1],
+            dataLabels: {
+                format: '<div style="text-align:center"><span style="font-size:25px;color:' +
+                    ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y:.1f}</span><br/>' +
+                       '<span style="font-size:12px;color:silver">A</span></div>'
+            },
+            tooltip: {
+                valueSuffix: ' A'
+            }
+        }]
+
+    }));
+});
 
